e8928ea3a02691bacaca9c2edb98583753bd0975
ryan tulino <rtulino@weblinc.com>
2019-04-27 13:53:59 -0400

    install magento from openmage

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # magento-installer
 Programmatically Install Magento v1
+
+## Install
+
+```
+composer require ryaan_anthony/magento-installer
+```
+
+## Usage
+
+* Instantiate the installer with default values
+
+```  
+  $magentoInstaller = new RyaanAnthony\MagentoInstaller();
+```
+
+* or configure the installer yourself
+
+```
+  $magentoInstaller = new RyaanAnthony\MagentoInstaller(
+    'https://example.com/magento.tar.gz'
+    '/path/to/magento/',
+    'magento.tar.gz'
+  );   
+```
+
+* Download and extract Magento 
+
+```
+  $magentoInstaller->setUp();
+```
+
+* Get the Magento application directory
+
+```
+  $magentoInstaller->getDirectory();
+```
+
+## Caveats
+
+* Only works with gzipped tar files
+
+- - -
+Tested using PHP7.2
--- src/MagentoInstaller.php
+++ src/MagentoInstaller.php
@@ -4,8 +4,53 @@ namespace RyaanAnthony;
 
 class MagentoInstaller
 {
-  public function setUp()
+  const MAGENTO_DIR = __DIR__.'/../magento/';
+  const ARCHIVE_NAME = 'magento.tar.gz';
+  const ARCHIVE_URL = 'https://github.com/OpenMage/magento-mirror/archive/1.9.4.1.tar.gz';
+
+  function __construct(
+    string $directory = self::MAGENTO_DIR,
+    string $archive_name = self::ARCHIVE_NAME,
+    string $archive_url = self::ARCHIVE_URL
+  ) {
+    $this->directory = $directory;
+    $this->archive_url = $archive_url;
+    $this->archive_name = $archive_name;
+  }
+
+  public function setUp() {
+    try {
+      $this->downloadFile();
+      $this->extractFile();
+      $this->cleanUpFile();
+
+      print 'Setup complete!'.PHP_EOL;
+    } catch (\Exception $e) {
+      print 'Error occurred during setup!!'.PHP_EOL;
+      print $e->getMessage().PHP_EOL;
+    }
+  }
+
+  public function getDirectory()
   {
-    print "Setup complete!";
+    return glob($this->directory . '*')[0];
+  }
+
+  protected function downloadFile()
+  {
+    copy($this->archive_url, $this->archive_name);
+  }
+
+  protected function extractFile()
+  {
+    $phar = new \PharData($this->archive_name);
+    $phar->decompress();
+    $phar->extractTo($this->directory);
+  }
+
+  protected function cleanUpFile()
+  {
+    unlink(str_replace('.gz', '', $this->archive_name));
+    unlink($this->archive_name);
   }
 }
